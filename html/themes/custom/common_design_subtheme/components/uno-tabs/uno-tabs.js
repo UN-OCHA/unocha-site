4567d6ba7f3a9c1218f21d2fabacfd0168100e91
orakili <github@orakili.net>
2023-04-14 05:36:49 +0000

    refactor: update uno tabs script to handle multiple tabbed sections in the page
    Refs: UNO-617

--- html/themes/custom/common_design_subtheme/components/uno-tabs/uno-tabs.js
+++ html/themes/custom/common_design_subtheme/components/uno-tabs/uno-tabs.js
@@ -1,81 +1,104 @@
 (function (Drupal) {
+
   'use strict';
 
   Drupal.behaviors.unoTabs = {
     attach: function (context, settings) {
-
-      // Get relevant elements and collections
-      const tabbed = document.querySelector('.uno-tabs');
-      const tablist = tabbed.querySelector('.uno-tabs ul');
-      const tabs = tablist.querySelectorAll('.uno-tabs ul a');
-      const panels = tabbed.querySelectorAll('[id^="section"]');
-
-      // The tab switching function
-      const switchTab = (oldTab, newTab) => {
+      // The tab switching function.
+      const switchTab = (tabs, panels, oldTab, newTab) => {
         newTab.focus();
-        // Make the active tab focusable by the user (Tab key)
+        // Make the active tab focusable by the user (Tab key).
         newTab.removeAttribute('tabindex');
         // Set the selected state
         newTab.setAttribute('aria-selected', 'true');
         oldTab.removeAttribute('aria-selected');
         oldTab.setAttribute('tabindex', '-1');
         // Get the indices of the new and old tabs to find the correct
-        // tab panels to show and hide
+        // tab panels to show and hide.
 
         let index = Array.prototype.indexOf.call(tabs, newTab);
         let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
         panels[oldIndex].hidden = true;
         panels[index].hidden = false;
-      }
+      };
 
-      // Add the tablist role to the first <ul> in the .uno-tabs container
-      tablist.setAttribute('role', 'tablist');
+      // Enhanced tabbed section.
+      const enhanceSection = (section) => {
+        section.setAttribute('data-uno-tabs-processed', '');
 
-      // Add semantics are remove user focusability for each tab
-      Array.prototype.forEach.call(tabs, (tab, i) => {
-        tab.setAttribute('role', 'tab');
-        tab.setAttribute('id', 'tab' + (i + 1));
-        tab.setAttribute('tabindex', '-1');
-        tab.parentNode.setAttribute('role', 'presentation');
+        // Get relevant elements and collections.
+        const tablist = section.querySelector('.uno-tabs ul');
+        const tabs = tablist.querySelectorAll('.uno-tabs ul a');
+        const panels = section.querySelectorAll('[id^="section"]');
 
-        // Handle clicking of tabs for mouse users
-        tab.addEventListener('click', e => {
-          e.preventDefault();
-          let currentTab = tablist.querySelector('[aria-selected]');
-          if (e.currentTarget !== currentTab) {
-            switchTab(currentTab, e.currentTarget);
-          }
-        });
+        // Add the tablist role to the first <ul> in the .uno-tabs container.
+        tablist.setAttribute('role', 'tablist');
 
-        // Handle keydown events for keyboard users
-        tab.addEventListener('keydown', e => {
-          // Get the index of the current tab in the tabs node list
-          let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
-          // Work out which key the user is pressing and
-          // Calculate the new tab's index where appropriate
-          let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
-          if (dir !== null) {
+        // Add semantics are remove user focusability for each tab.
+        Array.prototype.forEach.call(tabs, (tab, i) => {
+          tab.setAttribute('role', 'tab');
+          tab.setAttribute('id', 'tab' + (i + 1));
+          tab.setAttribute('tabindex', '-1');
+          tab.parentNode.setAttribute('role', 'presentation');
+
+          // Handle clicking of tabs for mouse users.
+          tab.addEventListener('click', e => {
             e.preventDefault();
-            // If the down key is pressed, move focus to the open panel,
-            // otherwise switch to the adjacent tab
-            dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
-          }
+            let currentTab = tablist.querySelector('[aria-selected]');
+            if (e.currentTarget !== currentTab) {
+              switchTab(tabs, panels, currentTab, e.currentTarget);
+            }
+          });
+
+          // Handle keydown events for keyboard users.
+          tab.addEventListener('keydown', e => {
+            // Get the index of the current tab in the tabs node list.
+            let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
+            // Work out which key the user is pressing and calculate the new
+            // tab's index where appropriate.
+            let dir = null;
+            if (e.which === 37) {
+              dir = index - 1;
+            }
+            else if (e.which === 39) {
+              dir = index + 1;
+            }
+            else if (e.which === 40) {
+              dir = 'down';
+            }
+            if (dir !== null) {
+              e.preventDefault();
+              // If the down key is pressed, move focus to the open panel,
+              // otherwise switch to the adjacent tab.
+              if (dir === 'down') {
+                panels[i].focus();
+              }
+              else if (tabs[dir]) {
+                switchTab(tabs, panels, e.currentTarget, tabs[dir]);
+              }
+            }
+          });
         });
-      });
 
-      // Add tab panel semantics and hide them all
-      Array.prototype.forEach.call(panels, (panel, i) => {
-        panel.setAttribute('role', 'tabpanel');
-        panel.setAttribute('tabindex', '-1');
-        let id = panel.getAttribute('id');
-        panel.setAttribute('aria-labelledby', tabs[i].id);
-        panel.hidden = true;
-      });
+        // Add tab panel semantics and hide them all.
+        Array.prototype.forEach.call(panels, (panel, i) => {
+          panel.setAttribute('role', 'tabpanel');
+          panel.setAttribute('tabindex', '-1');
+          panel.setAttribute('aria-labelledby', tabs[i].id);
+          panel.hidden = true;
+        });
 
-      // Initially activate the first tab and reveal the first tab panel
-      tabs[0].removeAttribute('tabindex');
-      tabs[0].setAttribute('aria-selected', 'true');
-      panels[0].hidden = false;
+        // Initially activate the first tab and reveal the first tab panel.
+        tabs[0].removeAttribute('tabindex');
+        tabs[0].setAttribute('aria-selected', 'true');
+        panels[0].hidden = false;
+      };
+
+      // Enhance tabbed sections.
+      const sections = document.querySelectorAll('.uno-tabs:not([data-uno-tabs-processed])');
+      if (sections) {
+        Array.prototype.forEach.call(sections, enhanceSection);
+      }
     }
   };
 })(Drupal);
