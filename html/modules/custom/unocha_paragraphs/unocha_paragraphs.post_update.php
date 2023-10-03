0482d3fb2c9ce467efd1b04ae1ce4aace6356d83
orakili <github@orakili.net>
2023-10-03 00:28:53 +0000

    feat: use different view mode and template for featured stories when using cards with featured view mode
    Refs: UNO-785

---
+++ html/modules/custom/unocha_paragraphs/unocha_paragraphs.post_update.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Post update file for the unocha_paragraphs module.
+ */
+
+/**
+ * Implements hook_post_update_NAME().
+ *
+ * Change the view mode of the stories paragraphs.
+ */
+function unocha_paragraphs_post_update_stories_paragraph_view_mode(&$sandbox) {
+  $paragraphs = \Drupal::entityTypeManager()
+    ->getStorage('paragraph')
+    ->loadByProperties([
+      'type' => 'stories',
+    ]);
+
+  foreach ($paragraphs as $paragraph) {
+    $view_mode = $paragraph->field_limit->value == 4 ? 'cards_with_featured' : 'cards';
+    $paragraph->paragraph_view_mode->value = $view_mode;
+    $paragraph->setNewRevision(FALSE);
+    $paragraph->save();
+  }
+
+  $result = t('Updated view mode of %count stories paragraphs', [
+    '%count' => count($paragraphs),
+  ]);
+  return $result;
+}
