6c52efe261dfb3710ec6174fefee1ebb3f19d09e
left23 <lcdemc@gmail.com>
2024-03-04 17:22:47 +0100

    test: wait for parent elements, console log
    Refs: UNO-823

--- html/themes/custom/common_design_subtheme/_tests/SearchDropdown.test.js
+++ html/themes/custom/common_design_subtheme/_tests/SearchDropdown.test.js
@@ -6,8 +6,10 @@ describe('SearchDropdown', () => {
   });
 
   it('should expand when clicked', async() => {
+    await page.waitForSelector('.cd-search');
     const toggle = await page.$('.cd-search__btn');
     await toggle.click();
+    console.log("Search button clicked");
     const hidden = await page.$eval('.cd-search__form', el => el.dataset.cdHidden);
     await expect(hidden).toMatch('false');
   });
