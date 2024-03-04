465e0bf4999865c4dabb7f54b51016089cad16c5
left23 <lcdemc@gmail.com>
2024-03-04 16:37:56 +0100

    test: try wait until load
    Refs: UNO-823

--- html/themes/custom/common_design_subtheme/_tests/SearchDropdown.test.js
+++ html/themes/custom/common_design_subtheme/_tests/SearchDropdown.test.js
@@ -2,13 +2,12 @@ import env from './_env'
 
 describe('SearchDropdown', () => {
   beforeAll(async() => {
-    await page.goto(env.baseUrl);
+    await page.goto(env.baseUrl, { waitUntil: 'load' });
   });
 
   it('should expand when clicked', async() => {
-    let toggle = '.cd-search__btn';
-    await page.waitForSelector(toggle, {timeout: 3000});
-    await page.click(toggle);
+    const toggle = await page.$('.cd-search__btn');
+    await toggle.click();
     const hidden = await page.$eval('.cd-search__form', el => el.dataset.cdHidden);
     await expect(hidden).toMatch('false');
   });
