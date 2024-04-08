652bf702865876a53ae9a58a324a357f11a087e9
Peter Droogmans (attiks) <peter@attiks.com>
2024-04-08 11:04:39 +0200

    feat: Add datawrapper
    Refs: #UNO-829

---
+++ html/modules/custom/ocha_datawrapper/js/datawrapper-responsive-iframe.js
@@ -0,0 +1,16 @@
+/*
+ * Datawrapper script to handle responsive iframe.
+ *
+ * @see https://developer.datawrapper.de/docs/responsive-iframe
+ */
+window.addEventListener('message', function(event) {
+  if (typeof event.data['datawrapper-height'] !== 'undefined') {
+    for (var chartId in event.data['datawrapper-height']) {
+      var iframe =
+        document.getElementById('datawrapper-chart-' + chartId) ||
+        document.querySelector("iframe[src*='" + chartId + "']");
+      if (!iframe) continue;
+      iframe.style.height = event.data['datawrapper-height'][chartId] + 'px';
+    }
+  }
+});
